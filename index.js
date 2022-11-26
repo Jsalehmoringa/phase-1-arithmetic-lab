5750220075c0c6231327a5658ab9f6e8519f25b0
jamil <jamil.saleh@student.moringaschool.com>
2022-11-26 15:34:40 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+let num1 = 31
+let num2 = 2
+const multiply = num1 * num2
+console.log(multiply)
+// Random number 1-10
+const random = Math.floor(Math.random()*10) + 1
+console.log(random)
+//Calculate reminer equal to 4 
+let num3 = 12
+let num4 = 3
+const mod = num3 / num4
+console.log(mod)
+//Returns 20 as the highets number 
+const max = 20
+console.log(Math.max(max))
